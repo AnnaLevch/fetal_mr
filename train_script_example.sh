c18bdcf33b5c13613f211597ebe7d6617cce8e34
Anna Levchakova <annal@fmrin.gov.il>
2024-01-16 11:58:26 +0200

    removed bash scripts

--- train_script_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/csh
-
-
-echo 'starting train script' | mutt -s 'starting train script' --
-
-python3 train.py with /cs/labs/josko/bella_fadida/code/code_bella/fetal_mr/config/config_body/config_FIESTA_small_large_fetuses.json > log_train_1.txt
-
